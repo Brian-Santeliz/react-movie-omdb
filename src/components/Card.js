a6084e862f30dc034a971a7aeafecd324b2e90a3
Brian <blsvsanteliz@gmail.com>
2020-08-22 22:53:49 -0400

    Creating layout in list

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Card = ({movie}) => {
-    const {Title,Year,Type, Poster } = movie
-    return ( 
-        <>
+const Card = ({ movie }) => {
+  const { Title, Year, Type, Poster } = movie;
+  return (
+    <>
+
             <div className="card">
-                <div className="card-body">
-                    <img src={Poster} alt=""/>
-                </div>
+              <div className="card-body">
+                <img src={Poster} alt={Title} className="img-fluid" />
+              </div>
             </div>
-        </>
-     );
-}
- 
+    </>
+  );
+};
+
 export default Card;
--- src/components/List.js
+++ src/components/List.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
+const List = ({ movies }) => {
+  if (movies.length === 0) return null;
 
-const List = ({movies}) => {
-    if(movies.length === 0) return null
-   
-    return ( 
-        <div>
-            {movies.map(movie=>
-               
-                <Card
-                    key={movie.imdbID}
-                    movie={movie}
-                />  
-               
-            )}
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          {movies.map((movie) => (
+          <div className="col-md-4">
+            <Card key={movie.imdbID} movie={movie} />
+            </div>
+          ))}
         </div>
-     );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default List;
