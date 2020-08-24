caa022440a7ea8d6a33d49b9e24802e1e9106732
Brian <blsvsanteliz@gmail.com>
2020-08-23 22:55:38 -0400

    Modify layout of card

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,14 @@ const Card = ({ movie }) => {
   return (
     <>
     <Fade>
-      <div className="card my-3 card-color">
+      <div className="card my-3 card-color ">
         <img src={Poster} alt={Title} className="img-fluid card-img-top " />
         <div className="card-body">
-          <h5 className="text-center">{Title}</h5>
-          <div className="d-flex justify-content-between ">
+          <div className="card-title">
+              <h5 className="text-center ">{Title}</h5>
+          </div>
+        <a className=" btn-info btn-block my-1 card-text">More info</a>
+          <div className="d-flex justify-content-between mt-2">
             <small className="font-weight-bold">Year: {Year}</small>
             <small className="font-weight-bold">Type: {Type}</small>
           </div>
