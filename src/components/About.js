2aec67a5264104ae94aa136f326caa4cb47ed1e4
Brian <blsvsanteliz@gmail.com>
2020-08-27 16:18:04 -0400

    Add useEffect, to fetch api

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import Header from './Header'
 import {useParams} from 'react-router-dom'
 
 const About = () => {
     const {id} = useParams();
+    const [about,setAbout] = useState([])
+    useEffect(()=>{
+        const fetchApi = async()=>{
+            const key = 'f05561ad'
+            const res = await fetch(`http://www.omdbapi.com/?apikey=${key}&i=${id}`)
+           const response = await  res.json()
+           setAbout(response)
+        }
+        fetchApi()
+    },[])
     return ( 
         <>
         <Header
