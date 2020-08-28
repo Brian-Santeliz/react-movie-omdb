97c2b8cff6a3ce2868b32a2c3c04cba58f57fc74
Brian <blsvsanteliz@gmail.com>
2020-08-27 23:48:42 -0400

    layout finish

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchApi } from "../helper";
-
+import star from "./star.png";
 const About = () => {
   const { id } = useParams();
   const [about, setAbout] = useState({});
-  const [load,setLoad] = useState(false)
+  const [load, setLoad] = useState(false);
   useEffect(() => {
     const init = async () => {
-        setLoad(true)
+      setLoad(true);
       const res = await fetchApi(id);
       setAbout(res);
-      setLoad(false)
-      console.log(res)
+      setLoad(false);
     };
     init();
   }, [id]);
-  const { Poster, Title, Plot, Actors, Director, Year, Runtime , Genre} = about;
+  const {
+    Poster,
+    Title,
+    Plot,
+    Actors,
+    Director,
+    Year,
+    Runtime,
+    Genre,
+    imdbRating,
+  } = about;
   return (
     <>
       <div className="container">
         <div className="row">
-        {load && 'Loading...'}
+          {load && "Loading..."}
           <div className="col-md-6 mx-auto text-center">
             <img
               src={Poster}
@@ -30,13 +39,22 @@ const About = () => {
             />
           </div>
           <div className="col-md-6">
-            <h2 className="text-white my-4 text-center">{Title}</h2>
-        <p className="text-justify text-white">{Plot}</p>
-         <p className="text-justify text-white mt-2">{Director && `Directors: ${Director}`}</p>
-         <p className="text-justify text-white mt-2">{Actors && `Actors: ${Actors}`}</p>
-         <p className="text-justify text-white mt-2">{Year && `Year: ${Year}`}</p>
-         <p className="text-justify text-white mt-2">{Runtime && `Duration: ${Runtime}`}</p>
-         <p className="text-justify text-white mt-2">{Genre && `Genre: ${Genre}`}</p>
+            <h2 className="text-white my-4 text-center">
+              {Title && Year && `${Title} (${Year})`}
+            </h2>
+            <span className="text-white mb-1 size">
+              {imdbRating && <img src={star} alt="Rating" />}
+              <span> {imdbRating && `${imdbRating}/10`}</span>
+            </span>
+            <p className="text-justify text-white">{Plot}</p>
+            <p className="text-white mt-2">
+              {Director && `Directors: ${Director}`}
+            </p>
+            <p className="text-white mt-1">{Actors && `Actors: ${Actors}`}</p>
+            <p className="text-white mt-1">
+              {Runtime && `Duration: ${Runtime}`}
+            </p>
+            <p className="text-white mt-1">{Genre && `Genre: ${Genre}`}</p>
           </div>
         </div>
       </div>
