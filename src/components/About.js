441a7e235045b943e5ea425895603b39dac7ff55
Brian <blsvsanteliz@gmail.com>
2020-08-27 22:57:33 -0400

    Add state to loading & details

--- src/components/About.js
+++ src/components/About.js
@@ -1,35 +1,47 @@
-import React,{useEffect,useState} from 'react';
-import {useParams} from 'react-router-dom'
-import { fetchApi} from '../helper'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchApi } from "../helper";
 
 const About = () => {
-    const {id} = useParams();
-    const [about,setAbout] = useState({})
-    useEffect(()=>{
-       
-        const init = async()=>{
-        const res = await  fetchApi(id)
-        setAbout(res)
-        }
-            init()
-    },[id])
-    const {Poster, Title, Plot, Actors} = about
-    return ( 
-        <>
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6 mx-auto text-center">
-                    <img src={Poster} alt={Title} className="img-fluid  my-3 p-2 border"/>
-                </div> 
-                <div className="col-md-6">
-                  <h2 className="text-white my-3">{Title}</h2>
-
-                </div>   
-            </div>    
-        </div>  
-    
+  const { id } = useParams();
+  const [about, setAbout] = useState({});
+  const [load,setLoad] = useState(false)
+  useEffect(() => {
+    const init = async () => {
+        setLoad(true)
+      const res = await fetchApi(id);
+      setAbout(res);
+      setLoad(false)
+      console.log(res)
+    };
+    init();
+  }, [id]);
+  const { Poster, Title, Plot, Actors, Director, Year, Runtime , Genre} = about;
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+        {load && 'Loading...'}
+          <div className="col-md-6 mx-auto text-center">
+            <img
+              src={Poster}
+              alt={Title}
+              className="img-fluid  mt-3 p-2 borde"
+            />
+          </div>
+          <div className="col-md-6">
+            <h2 className="text-white my-4 text-center">{Title}</h2>
+        <p className="text-justify text-white">{Plot}</p>
+         <p className="text-justify text-white mt-2">{Director && `Directors: ${Director}`}</p>
+         <p className="text-justify text-white mt-2">{Actors && `Actors: ${Actors}`}</p>
+         <p className="text-justify text-white mt-2">{Year && `Year: ${Year}`}</p>
+         <p className="text-justify text-white mt-2">{Runtime && `Duration: ${Runtime}`}</p>
+         <p className="text-justify text-white mt-2">{Genre && `Genre: ${Genre}`}</p>
+          </div>
+        </div>
+      </div>
     </>
-     );
-}
- 
+  );
+};
+
 export default About;
