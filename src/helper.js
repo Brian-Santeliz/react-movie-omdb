af801d7396da4d48bc8718e64139dc18097ed977
Brian <blsvsanteliz@gmail.com>
2020-08-31 18:25:34 -0400

    modify http to https

--- src/helper.js
+++ src/helper.js
@@ -1,12 +1,12 @@
 const apiKey = process.env.REACT_APP_API_KEY;
 export const dataFetch = async(title) =>{
-const url = ` http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`;
+const url = ` https://www.omdbapi.com/?apikey=${apiKey}&s=${title}`;
 //query api
 const res = await fetch(url);
 return await res.json();
 }
 export const fetchApi = async(id)=>{
-    const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${id}`)
+    const res = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${id}`)
   return await res.json()
   
 }
